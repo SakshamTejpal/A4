e33f02c210de895673fcdbd6c17fbd95a65979c1
300932743 <300932743@PR-A315-40>
2017-07-24 19:07:43 -0400

    Created Planet class

---
+++ A4/Planet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name-Saksham Tejpal
+ * Date-7/24/17
+ * Description-This is Planet abstract class
+ * Version 0.1-Created Planet class
+ */ 
+namespace A4
+{
+    class Planet
+    {
+       
+    }
+}
