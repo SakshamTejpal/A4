2be17a0301a706f68f2fc71a14b2c562aa4a6432
300932743 <300932743@PR-A315-40>
2017-07-27 19:27:19 -0400

    Created GiantPlanet class

---
+++ A4/TerrestrialPlanet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name-Saksham Tejpal
+ * Date-7/24/17
+ * Description-This is TerrestrialPlanet class
+ * Version 0.2-Created GiantPlanet class
+ */
+namespace A4
+{
+    public class TerrestrialPlanet
+    {
+    }
+}
