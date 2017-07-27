ad8cda00caad1a4cfacaec2b3b5dc9398b7e3350
300932743 <300932743@PR-A315-40>
2017-07-27 19:56:00 -0400

    Created IHasRing Interface

---
+++ A4/IHasMoon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name-Saksham Tejpal
+ * Date-7/24/17
+ * Description-This is IHasMoon Interface
+ * Version 0.1-Created IHasMoon Interface
+ */
+namespace A4
+{
+    public interface IHasMoon
+    {
+        public bool HasMoons();
+    }
+}
---
+++ A4/IHasRing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name-Saksham Tejpal
+ * Date-7/24/17
+ * Description-This is IHasRing Interface
+ * Version 0.1-Created IHasRing Interface
+ */
+namespace A4
+{
+    public interface IHasRing
+    {
+        public bool HasRing();
+    }
+}
