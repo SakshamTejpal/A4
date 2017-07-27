f71ed16ac20b8156fd26b58d859310d6e63f7bad
300932743 <300932743@PR-A315-40>
2017-07-27 19:40:17 -0400

    Created Constuctor for GiantPlanet class

--- A4/TerrestrialPlanet.cs
+++ A4/TerrestrialPlanet.cs
@@ -5,13 +5,19 @@ using System.Text;
 /* Name-Saksham Tejpal
  * Date-7/24/17
  * Description-This is TerrestrialPlanet class
- * Version 0.2-Created GiantPlanet class
+ * Version 0.3-Created Constuctor for GiantPlanet class
  */
 namespace A4
 {
-    public class TerrestrialPlanet
+    public class TerrestrialPlanet : Planet 
     {
         //PRIVATE INSTANCE VARIABLES
         private bool _oxygen;
+        //CONSTRUCTOR
+        TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+           : base(name, diameter, mass)
+        {
+            oxygen = _oxygen;
+        }
     }
 }
